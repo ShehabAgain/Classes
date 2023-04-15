f2a943af8204b939e00aebb4d7035c9422fa1d07
Shehab222 <>
2023-04-15 19:44:59 -0400

    added email to writer

--- secondary.py
+++ secondary.py
@@ -56,6 +56,6 @@ def student_project(char):
 # for char in [Speaker1, Writer1]:
 #     student_project(char)
 # print(isinstance(Speaker1, object))
-Writer1 = Writer("BO", "DQEF")
+Writer1 = Writer("BO", "DQEF","KK")
 Speaker1 = Speaker('F', "FFW", 'j')
 print(Speaker1.email)
